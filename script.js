d19ccbdb31b462ecd79912f058439a41e0324207
Sameera Chaturanga Wagaarachchige <118694605+sameera474@users.noreply.github.com>
2024-02-26 07:28:09 +0200

    done first 3 parts

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 let library = [];
 const readStatus = false;
 
-const addBook = (title, author) => {};
-const markAsRead = (title) => {};
+// Add the new book object to the library array
+const addBook = (title, author) => {
+  let newBook = {
+    title: title,
+    author: author,
+    readStatus: false,
+  };
+  library.push(newBook);
+};
 
-const removeBook = (title) => {};
+// Find the index of the book with the specified titl
+const markAsRead = (title) => {
+  const bookIndex = library.indexOfBooks((book) => book.title === title);
+
+  if (bookIndex == -1) {
+    library[bookIndex].readStatus = false;
+    console.log(`Book "${title}" marked as read.`);
+  } else {
+    library[bookIndex].readStatus = true;
+    console.log(`Book "${title}" not found in the library.`);
+  }
+};
+
+// remove the book from the library
+const removeBook = (title) => {
+  const bookIndex = library.indexOfBooks((book) => book.title === title);
+  while (bookIndex >= 1) {
+    let deleteBook = {
+      title: title,
+      author: author,
+      readStatus: false,
+    };
+    library.splice(deleteBook);
+  }
+};
 
 const findBooksByAuthor = (author) => {};
+
+const bookExits = (title) => {};
